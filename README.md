94685dee17dc998472fbd825661e445ed4c70843
chidrome <chidrome@gmail.com>
2019-07-03 11:45:15 -0700

    added put routes

--- README.md
+++ README.md
@@ -3,3 +3,49 @@
 ## Description
 Taskmaster is a full stack web app that allows users to enter and track tasks accordingly.
 
+
+**Running the app:**
+
+Deployed:
+* wip
+
+Locally:
+- git clone this repo
+- In an IDE open the Taskmaster project and run the TaskmasterApplication class
+
+***
+
+**Routes:**
+
+GET:
+http://localhost:5000/tasks
+- Returns all tasks in JSON format
+
+GET:
+http://localhost:5000/users/{name}/tasks
+- Returns tasks assigned to a user or empty array if user not found
+
+POST:
+http://localhost:5000/tasks?title=mytitle&assignee=name&description=thedesc
+- Adds a task and returns the task as JSON
+
+PUT:
+http://localhost:5000/tasks/{id}/state
+- Advances a task from: Available -> Assigned -> Accepted -> Finished
+
+PUT:
+http://localhost:5000//tasks/{id}/assign/{assignee}
+- Assigns a task to assignee and sets status to assigned
+***
+
+**Code**
+
+[Task Model class](src/main/java/com/chidrome/taskmaster/taskmaster/models/TaskInfo.java)
+
+[Task Controller class](src/main/java/com/chidrome/taskmaster/taskmaster/controllers/TaskmasterController.java)
+***
+
+**Testing**
+
+[Integration test class](src/test/java/com/esa/taskmaster/taskmaster/TaskMasterRepositoryIntegrationTest.java)
+***
--- src/main/java/com/chidrome/taskmaster/taskmaster/controllers/TaskmasterController.java
+++ src/main/java/com/chidrome/taskmaster/taskmaster/controllers/TaskmasterController.java
@@ -2,10 +2,13 @@ package com.chidrome.taskmaster.taskmaster.controllers;
 
 import com.chidrome.taskmaster.taskmaster.models.TaskInfo;
 import com.chidrome.taskmaster.taskmaster.repositories.TaskRepository;
+import org.assertj.core.util.Lists;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 public class TaskmasterController {
@@ -20,14 +23,46 @@ public class TaskmasterController {
     }
 
     @PostMapping("/tasks")
-    public String createTask(String title, String description, String status){
+    public ResponseEntity<String> createTask(@RequestParam String title, @RequestParam String description, @RequestParam String assignee){
         // Status should always start out as available
-        return "created";
+        TaskInfo newTask = new TaskInfo(title, description, assignee, "available"); //all new tasks should start with available as status
+        taskRepository.save(newTask);
+        return ResponseEntity.ok("Task(s) created");
     }
 
 
     @PutMapping("/tasks/{id}/state")
-    public String updateStatus(@PathVariable String id){
-        return "udpated";
+    public ResponseEntity<String> updateStatus(@PathVariable String id){
+        TaskInfo taskToUpdate = taskRepository.findById(id).get();
+        if(taskToUpdate.getStatus().toLowerCase().equals("available")){
+            taskToUpdate.setStatus("assigned");
+        } else if(taskToUpdate.getStatus().toLowerCase().equals("assigned")){
+            taskToUpdate.setStatus("accepted");
+        } else {
+            taskToUpdate.setStatus("completed");
+        }
+        taskRepository.save(taskToUpdate);
+        return ResponseEntity.ok("Task has been updated");
     }
+
+    @GetMapping("/users/{name}/tasks")
+    public ResponseEntity<List> getUserTasks(@PathVariable String name){
+        Iterable<TaskInfo> tasks = taskRepository.findAll();
+        List<String> assigneeList = Lists.newArrayList();
+
+        for (TaskInfo t : tasks) {
+            if (t.getAssignee() != null) if (t.getAssignee().equals(name)) assigneeList.add(t.getDescription());
+        }
+        return ResponseEntity.ok(assigneeList);
+    }
+
+    @PutMapping("/tasks/{id}/assign/{assignee}")
+    public ResponseEntity<TaskInfo> updateAssignee(@PathVariable String id, @PathVariable String assignee){
+        TaskInfo task = taskRepository.findById(id).get();
+        task.setAssignee(assignee);
+        task.setStatus("assigned");
+        taskRepository.save(task);
+        return ResponseEntity.ok(task);
+    }
+
 }
